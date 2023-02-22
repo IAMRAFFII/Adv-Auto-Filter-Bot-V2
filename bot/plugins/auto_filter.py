ee7ed6680ce9c45f14214eecbaff37e20f7c8282
Rafi <89293629+IAMRAFFII@users.noreply.github.com>
2023-02-22 14:07:40 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -81,7 +81,7 @@ async def auto_filter(bot, update):
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"{file_size}{file_name}"
+            button_text = f"🔰{file_size}{file_name}🔰"
             
 
             if file_type == "video":
@@ -147,13 +147,13 @@ async def auto_filter(bot, update):
         if len_result != 1:
             result[0].append(
                 [
-                    InlineKeyboardButton("NEXT🔥", callback_data=f"navigate(0|next|{query})")
+                    InlineKeyboardButton("NEXT➡️", callback_data=f"navigate(0|next|{query})")
                 ]
             )
         
         # Just A Decaration
         result[0].append([
-            InlineKeyboardButton(f"❣️ Page 1/{len_result if len_result < max_pages else max_pages} ✨️", callback_data="ignore")
+            InlineKeyboardButton(f"🔰Page 1/{len_result if len_result < max_pages else max_pages}✨️", callback_data="ignore")
         ])
         
         
